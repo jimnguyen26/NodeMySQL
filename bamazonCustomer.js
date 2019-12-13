e84b0d2567047632e3fddac62c0c5554c9bb60eb
Jim <jimmybnguyen@yahoo.com>
2019-12-13 14:13:17 -0800

    mysql connected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,3 +3,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Saigon26",
+    database: "bamazon_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("successful");
+    
+});
